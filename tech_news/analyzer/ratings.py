9af9bd2896937c3b23fa7af9f3e32c8dfd7e4ee0
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-30 12:00:01 -0300

    requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,25 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    noticias = find_news()
+    # https://pt.stackoverflow.com/questions/371321/itemgetter-ordenar-lista-de-dicion%C3%A1rios-python
+    ordered = sorted(
+        noticias,
+        key=lambda row: (row["comments_count"], row["title"]),
+        reverse=1,
+    )
+
+    result = []
+    if len(ordered) < 5:
+        for o in ordered:
+            result.append((o["title"], o["url"]))
+    else:
+        for c in range(5):
+            result.append((ordered[c]["title"], ordered[c]["url"]))
+            c += 1
+    return result
 
 
 # Requisito 11
