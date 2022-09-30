3320623b8859542e987b8f1d678134e6594d27b9
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-30 09:36:46 -0300

    requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 # Requisito 6
+import datetime
 from tech_news.database import search_news
 
 
@@ -12,7 +13,16 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    pass
+    try:
+        data = datetime.date.fromisoformat(date)
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        response = search_news({"timestamp": data.strftime("%d/%m/%Y")})
+        result = []
+        for r in response:
+            result.append((r["title"], r["url"]))
+        return result
 
 
 # Requisito 8
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -68,6 +68,3 @@ def get_tech_news(amount):
     create_news(noticias)
 
     return noticias
-
-
-get_tech_news(3)
